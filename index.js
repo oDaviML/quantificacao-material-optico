7c3ecff4f97d50014e760f1ce4f47a30af3d12f0
oDaviML <85958184+oDaviML@users.noreply.github.com>
2023-07-15 15:44:54 -0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function updateBackboneLimit() {
+    var numeroFibras = document.getElementById("numeroFibras");
+    var quantidadeBackbones = document.getElementById("quantidadeBackbones");
+    quantidadeBackbones.max = numeroFibras.value;
+}
+
 function calcularQuantificacao() {
     let numeroPavimentos = document.querySelector("#numeroPavimentos").value;
     let numeroFibras = document.querySelector("#numeroFibras").value;
@@ -10,37 +16,57 @@ function calcularQuantificacao() {
     let tabelaResultado = document.querySelector("#resultado");
     tabelaResultado.innerHTML = "";
 
-    let caboFibra = (numeroPavimentos * numeroFibras) + (numeroFibras * tamanhoPeDireito);
-    let acopladores = (numeroPavimentos * numeroFibras) / 2;
-    let terminadores = Math.ceil((numeroPavimentos * numeroFibras) / numeroFibras);
-    let cordoes = (numeroPavimentos * numeroFibras) / 2;
-    let pigtails = (numeroPavimentos * numeroFibras) / 2;
+    let tabelaResultadoMiscelania = document.querySelector("#resultadoMiscelania");
+    tabelaResultadoMiscelania.innerHTML = "";
+
+
+    let acopladores, cordoes, pigtails;
+
+    if (caracteristicaFibra === "Duplo") {
+        acopladores = (numeroPavimentos * quantidadeBackbones) / 2;
+        cordoes = (numeroPavimentos * quantidadeBackbones) / 2;
+        pigtails = (numeroPavimentos * quantidadeBackbones) / 2;
+    }
+    else {
+        acopladores = (numeroPavimentos * quantidadeBackbones);
+        cordoes = (numeroPavimentos * quantidadeBackbones);
+        pigtails = (numeroPavimentos * quantidadeBackbones);
+    }
+    let caboFibra;
+
+    if (verificacaoBackbone) {
+        caboFibra = (numeroPavimentos * numeroFibras) + (numeroFibras * tamanhoPeDireito) * 2;
+    } else {
+        caboFibra = (numeroPavimentos * numeroFibras) + (numeroFibras * tamanhoPeDireito);
+    }
     
-    let dio = Math.ceil((numeroPavimentos * numeroFibras) / 24);
+    let terminadores = Math.ceil((numeroPavimentos * quantidadeBackbones) / quantidadeBackbones);
+    let dio = Math.ceil((numeroPavimentos * quantidadeBackbones) / 24);
+    let bandejaEmenda = Math.ceil((numeroPavimentos * quantidadeBackbones) / 12);
+    let etiquetasCOP = (numeroPavimentos * quantidadeBackbones);
+    let etiquetasDIO = (numeroPavimentos * quantidadeBackbones);
 
     if (especificacaoCabo === "Multimodo 50/125μm") {
         adicionarResultado(1, "Cabo de Fibra Óptica (FOMMIG) 50/125μm", caboFibra, "Metros");
         adicionarResultado(2, "Caixa DIO (24 portas)", dio, "Unidades");
-        adicionarResultado(3, "Acopladores Ópticos 50/125μm - Duplo - Lc", acopladores, "Unidades");
-        adicionarResultado(4, "Terminadores Ópticos", terminadores, "Unidades");
-        adicionarResultado(5, "Cordões Ópticos 50/125μm - Duplo - Lc", cordoes, "Unidades");
-        adicionarResultado(6, "Pigtails 50/125μm - Duplo - Lc", pigtails, "Unidades");
+        adicionarResultado(3, "Bandeja emenda (12 emendas)", bandejaEmenda, "Unidades");
+        adicionarResultado(4, "Acopladores Ópticos 50/125μm - "+ caracteristicaFibra +" - Lc", acopladores, "Unidades");
+        adicionarResultado(5, "Terminadores Ópticos (8 fibras)", terminadores, "Unidades");
+        adicionarResultado(6, "Cordões Ópticos 50/125μm - "+ caracteristicaFibra +" - Lc", cordoes, "Unidades");
+        adicionarResultado(7, "Pigtails 50/125μm - "+ caracteristicaFibra +" - Lc", pigtails, "Unidades");
+        adicionarResultadoMiscelania(1, "Etiqueta para os cordões ópticos e pigtails", etiquetasCOP, "Unidades");
+        adicionarResultadoMiscelania(2, "Etiqueta para Portas do DIO", etiquetasDIO, "Unidades");
     }
     else {
         adicionarResultado(1, "Cabo de Fibra Óptica (FOSMIG) 9/125μm", caboFibra, "Metros");
         adicionarResultado(2, "Caixa DIO (24 portas)", dio, "Unidades");
-        adicionarResultado(3, "Acopladores Ópticos 9/125μm - Duplo - Lc", acopladores, "Unidades");
-        adicionarResultado(4, "Terminadores Ópticos", terminadores, "Unidades");
-        adicionarResultado(5, "Cordões Ópticos 9/125μm - Duplo - Lc", cordoes, "Unidades");
-        adicionarResultado(6, "Pigtails 9/125μm - Duplo - Lc", pigtails, "Unidades");
-    }
-
-    
-    
-
-    if (verificacaoBackbone) {
-        let backbones = numeroPavimentos * quantidadeBackbones;
-        adicionarResultado(7, "Backbones", backbones, "Unidades");
+        adicionarResultado(3, "Bandeja emenda (até 12)", bandejaEmenda, "Unidades");
+        adicionarResultado(4, "Acopladores Ópticos 9/125μm - "+ caracteristicaFibra +" - Lc", acopladores, "Unidades");
+        adicionarResultado(5, "Terminadores Ópticos", terminadores, "Unidades");
+        adicionarResultado(6, "Cordões Ópticos 9/125μm - "+ caracteristicaFibra +" - Lc", cordoes, "Unidades");
+        adicionarResultado(7, "Pigtails 9/125μm - "+ caracteristicaFibra +" - Lc", pigtails, "Unidades");
+        adicionarResultadoMiscelania(1, "Etiqueta para os cordões ópticos e pigtails", etiquetasCOP, "Unidades");
+        adicionarResultadoMiscelania(2, "Etiqueta para Portas do DIO", etiquetasDIO, "Unidades");
     }
 
     function adicionarResultado(numero, item, quantidade, medida) {
@@ -53,6 +79,18 @@ function calcularQuantificacao() {
             </tr>
         `;
         tabelaResultado.appendChild(linha);
+    }
+
+    function adicionarResultadoMiscelania(numero, item, quantidade, medida) {
+        let linha = document.createElement("tr");
+        linha.innerHTML = `
+                <th scope="row">${numero}</th>
+                <td>${item}</td>
+                <td>${quantidade}</td>
+                <td>${medida}</td>
+            </tr>
+        `;
+        tabelaResultadoMiscelania.appendChild(linha);
     }
 }
 
